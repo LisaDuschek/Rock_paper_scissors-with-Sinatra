b7563ce78356b3b5b7a93b4b432c37c421917411
Guest User <student@epicodus.com>
2015-08-05 13:23:23 -0700

    implement logic for rock paper scissors

--- lib/beats.rb
+++ lib/beats.rb
@@ -4,9 +4,12 @@ class String
   define_method(:beats) do |player|
 
     if ((self == "rock" && player == "scissors") ||
-       (self == "scissors" && player =="paper") ||
-       (self =="paper" && player == "rock"))
+        (self == "scissors" && player =="paper") ||
+        (self =="paper" && player == "rock"))
        true
+     elsif
+       self == player
+        "tie"
      else
        false
      end
